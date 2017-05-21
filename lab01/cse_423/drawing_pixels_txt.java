285b104d9b186d8cb14961e79e35e3d163ef60f0
imranfarid <imran.cyborg@gmail.com>
2017-05-21 22:32:10 +0600

    lab02 DDA line algorithm

--- lab01/cse_423/drawing_pixels_txt.java
+++ lab01/cse_423/drawing_pixels_txt.java
@@ -14,7 +14,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Random;
 
 
 public class drawing_pixels_txt implements GLEventListener{
@@ -63,7 +62,7 @@ public class drawing_pixels_txt implements GLEventListener{
     		  System.out.println("x: " + x + " " + "y: " + y);    		  
     	  }
     	  gl.glEnd();
-    	  
+    	  br.close();
       }catch(IOException e){
     	  e.printStackTrace();
       }    
---
+++ lab02/cse_423/dda_algorithm.java
@@ -0,0 +1,134 @@
+package cse_423;
+
+import java.util.Random;
+
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+//import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
+
+import javax.swing.JFrame;
+
+
+public class dda_algorithm implements GLEventListener{
+    static GLProfile profile = GLProfile.get(GLProfile.GL2);
+    static GLCapabilities capabilities = new GLCapabilities(profile);
+    // The canvas
+    static GLCanvas glcanvas = new GLCanvas(capabilities);
+    
+   public static void main(String[] args) {
+          //getting the capabilities object of GL2 profile
+             
+       
+             dda_algorithm l = new dda_algorithm();
+          //creating frame
+          glcanvas.addGLEventListener(l);
+          glcanvas.setSize(600, 400);
+          
+          final JFrame frame = new JFrame ("straight Line");
+          //adding canvas to frame
+          frame.getContentPane().add(glcanvas);
+          frame.setSize(frame.getContentPane().getPreferredSize());
+          frame.setVisible(true);
+          
+       }
+   public void display(GLAutoDrawable drawable) {
+      final GL2 gl = drawable.getGL().getGL2();
+      
+      Random rand = new Random();
+      int rangeMin = -1;
+      int rangeMax = 1;
+      float m, x_init, x_next, y_next;
+
+             //gl.glBegin (GL2.GL_POINTS);//static field
+             for(int i = 0; i < 1; i++){
+                 //random float between -1 & 1
+                 float x1 = rangeMin + (rangeMax - rangeMin) * rand.nextFloat();
+                 float y1 = rangeMin + (rangeMax - rangeMin) * rand.nextFloat();
+                 float x2 = rangeMin + (rangeMax - rangeMin) * rand.nextFloat();
+                 float y2 = rangeMin + (rangeMax - rangeMin) * rand.nextFloat();
+                 System.out.println("x1: " + x1 + " " + "y1: " + y1 + " x2: " + x2 + " " + "y2: " + y2 + " i: " + i);
+                 
+                 if(x1 > x2){
+                	 System.out.println("x1 is greater");
+                 }else{
+                	 System.out.println("x2 is greater");
+                 }
+               
+	             
+                 
+                 if(x1 > x2){
+	                   float temp = x1;
+	                   x1 = x2;
+	                   x2 = temp;
+	                   //temp = 0.0;
+	                   temp = y1;
+	                   y1 = y2;
+	                   y2 = temp;
+	                   
+	                   //calculate slope
+	                   m = ((y2-y1)/(x2-x1));
+	                   System.out.println("x1 is greater " + m);
+	               }else{
+	                  //m = (y2-y1)/(x2-x1);
+	            	   m = ((y2-y1)/(x2-x1));
+	            	   System.out.println("x2 is greater " + m);
+	               }
+	               System.out.println("end of for");
+	               
+	               if(m > -1 && m < 1){
+	                  x_init = x1;
+	                   while(x1 < x2){
+	                       
+	                       x_next = (float) (x1 + (1* 0.000001)) ;
+	                       y_next = (float) (y1 + (m * 0.000001)) ;
+	                       System.out.println("loop1 m: " + m + " " + "x: " + x_next + " " + "y: " + y_next);
+	                      gl.glBegin (GL2.GL_POINTS);
+	                      gl.glVertex2d(x_next, y_next);
+	                      gl.glEnd();
+	                      x1 = x_next;
+	                      y1 = y_next;
+	                      
+	                   }
+	               }else{
+	                  x_init = x1;
+	                  while(y1 < y2){
+	                      
+	                      x_next = (float) (x1 + ((1/m) * 0.000001));
+	                      y_next = (float) (y1 + (1 * 0.000001)) ;
+	                      System.out.println("loop2 m: " + m + " " + "x: " + x_next + " " + "y: " + y_next);
+	                      gl.glBegin (GL2.GL_POINTS);
+	                     gl.glVertex2d(x_next, y_next);
+	                     gl.glEnd();
+	                     x1 = x_next;
+	                     y1 = y_next;
+	                     
+	                  }
+	               }
+	               
+               
+             }
+          
+          
+      
+   }
+   
+   public void dispose(GLAutoDrawable arg0) {
+      //method body
+   }
+
+   
+   public void init(GLAutoDrawable drawable) {
+      // method body
+       //4. drive the display() in a loop
+        }
+   
+   public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
+      // method body
+   }
+   //end of main
+}//end of classimport javax.media.opengl.GL2;
+
