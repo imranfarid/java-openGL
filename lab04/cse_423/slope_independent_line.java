c4b9c29e49e3aaa4dfd65a872ba406eb9ccbd4f2
imranfarid <imran.cyborg@gmail.com>
2017-06-12 22:07:27 +0600

    slope independent line algorithm lab04 final

--- lab04/cse_423/slope_independent_line.java
+++ lab04/cse_423/slope_independent_line.java
@@ -22,7 +22,7 @@ public class slope_independent_line implements GLEventListener{
           //getting the capabilities object of GL2 profile
              
        
-	   	  slope_independent_line l = new slope_independent_line();
+             slope_independent_line l = new slope_independent_line();
           //creating frame
           glcanvas.addGLEventListener(l);
           glcanvas.setSize(600, 400);
@@ -37,99 +37,148 @@ public class slope_independent_line implements GLEventListener{
    
    
    public void generate_coord(){
-	   
+       
    }
    public void display(GLAutoDrawable drawable) {
       final GL2 gl = drawable.getGL().getGL2();
       
-	   double x, y, x0, x1, y0, y1, dx, dy, temp, d_init, dNE, dE;
-	   int zone;
-	      //coordinate for line AB
-	      x0 = 10;
-	      x1 = 30;
-	      y0 = 10;
-	      y1 = 40;
-	      dx = x1- x0;
-		  dy = y1 - y0;
-	      
-	      zone = det_zone(x0, y0, x1, y1);
-	      System.out.println("zone: " + zone);
-	      if(zone == -1){
-	    	  System.out.println("Belongs to no zone");
-	    	  
-	      }
-	      
-	      if(zone == 1 || zone == 2 || zone == 5 || zone == 6){
-	    	  
-	    	  //swapping x0 & y0
-	    	  temp = x0;
-	    	  x0 = y0;
-	    	  y0 = temp;
-	    	  
-	    	  //swapping x1 & y1
-	    	  temp = x1;
-	    	  x1 = y1;
-	    	  y1 = temp;
-	    	  
-	    	  System.out.println("Belongs to zone 1, 2, 5 or 6");
-	      }else{
-	    	  System.out.println("Doesn't belongs to zone 1, 2, 5 or 6");  
-	      }
-	      
-	      d_init = (2*dy)-dx;
-		  dE = 2*dy;
-		  dNE = 2*(dy - dx);
-		  x = x0;
-		  y = y0;
-		  System.out.println("Entering loop");
-		  while(x < x1){
-			  if(d_init < 0){
-				  d_init += dE;
-			  }else{
-				  d_init += dNE;
-				  y++;
-			  }
-			  x++;
-			  
-			  //convBack(x, y, zone);
-			  System.out.println("x: "+x +" "+"y: "+y);
-			  //convBack & draw
-			  gl.glBegin (GL2.GL_POINTS);
-			  if(zone == 0){
-				  gl.glColor3f( 1.0f, 0.0f, 0.0f ); //red
-				  gl.glVertex2d(x, y);
+       double x, y, x0, x1, y0, y1, dx, dy, temp, d_init, dNE, dE;
+       int zone;
+          //coordinate for line AB
+             //zone 1 green
+//          x0 = 10*0.01;
+//          x1 = 40*0.01;
+//          y0 = 10*0.01;
+//          y1 = 80*0.01;
+          
+          
+             //zone 2 blue
+//          x0 = -10*0.01;
+//          x1 = -40*0.01;
+//          y0 = 10*0.01;
+//          y1 = 80*0.01;
+          
+        //zone 3 magenta
+//          x0 = -10*0.01;
+//          x1 = -80*0.01;
+//          y0 = 10*0.01;
+//          y1 = 40*0.01;
+          
+        //zone 4 purple
+//          x0 = -10*0.01;
+//          x1 = -80*0.01;
+//          y0 = -10*0.01;
+//          y1 = -40*0.01;
+          
+        //zone 5 yellow
+          x0 = -10*0.01;
+          x1 = -40*0.01;
+          y0 = -10*0.01;
+          y1 = -80*0.01;
+          
+        //zone 6 gray
+//          x0 = 10*0.01;
+//          x1 = 40*0.01;
+//          y0 = -10*0.01;
+//          y1 = -80*0.01;
+          
+        //zone 7 teal
+//          x0 = 10*0.01;
+//          x1 = 80*0.01;
+//          y0 = -10*0.01;
+//          y1 = -40*0.01;
+          
+          //dx = x1- x0;
+          dx = Math.abs(x1)- Math.abs(x0);
+          //dy = y1 - y0;
+          dy = Math.abs(y1)- Math.abs(y0);
+          
+          zone = det_zone(x0, y0, x1, y1);
+          System.out.println("zone: " + zone);
+          if(zone == -1){
+              System.out.println("Belongs to no zone");
+              
+          }
+          
+          if(zone == 1 || zone == 2 || zone == 5 || zone == 6){
+              
+              //swapping x0 & y0
+              temp = x0;
+              x0 = y0;
+              y0 = temp;
+              
+              //swapping x1 & y1
+              temp = x1;
+              x1 = y1;
+              y1 = temp;
+              
+              System.out.println("Belongs to zone 1, 2, 5 or 6");
+          }else{
+              System.out.println("Doesn't belongs to zone 1, 2, 5 or 6");  
+          }
+          
+          d_init = ((2*dy)-dx)*0.01;
+          dE = (2*dy)*0.01;
+          dNE = (2*(dy - dx))*0.01;
+          x = x0;
+          y = y0;
+          
+          System.out.println("dx: "+ dx +" "+"dy: "+dy);
+          System.out.println("y: "+ y +" "+"y1: "+y1);
+          System.out.println("x: "+ x +" "+"x1: "+x1);
+          System.out.println("Entering loop");
+          
+          while(Math.abs(x) < Math.abs(x1)){
+              System.out.println("d_init: "+ d_init);
+              if(d_init < 0){
+                  d_init += dE;
+              }else{
+                  d_init += dNE;
+                  //y++;
+                  y = y + (1*0.001);
+              }
+              //x++;
+              x = x + (1*0.001);
+              
+              //convBack(x, y, zone);
+              System.out.println("x: "+x +" "+"y: "+y);
+              //convBack & draw
+              gl.glBegin (GL2.GL_POINTS);
+              if(zone == 0){
+                  gl.glColor3f( 1.0f, 0.0f, 0.0f ); //red
+                  gl.glVertex2d(x, y);
                   gl.glEnd();
-			   }else if(zone == 1){
-				   gl.glColor3f( 0.0f,1.0f,0.0f); //green
-				   gl.glVertex2d(x, y);
-				   gl.glEnd(); 
-			   }else if(zone == 2){
-				   gl.glColor3f( 0.0f,0.0f,1.0f ); //red
-				   gl.glVertex2d(x, y);
-	               gl.glEnd();
-			   }else if(zone == 3){
-				   gl.glColor3f( 1.0f, 0.0f, 0.0f ); //red
-				   gl.glVertex2d(x, y);
-	               gl.glEnd();
-			   }else if(zone == 4){
-				   gl.glColor3f( 1.0f, 0.0f, 0.0f ); //red
-				   gl.glVertex2d(x, y);
-	               gl.glEnd();
-			   }else if(zone == 5){
-				   gl.glColor3f( 1.0f, 0.0f, 0.0f ); //red
-				   gl.glVertex2d(x, y);
-	               gl.glEnd();
-			   }else if(zone == 6){
-				   gl.glColor3f( 1.0f, 0.0f, 0.0f ); //red
-				   gl.glVertex2d(x, y);
-	               gl.glEnd();
-			   }else if(zone == 7){
-				   gl.glColor3f( 1.0f, 0.0f, 0.0f ); //red
-				   gl.glVertex2d(x, y);
-	               gl.glEnd();
-			   }
-		  }//end while
-
+               }else if(zone == 1){
+                   gl.glColor3f( 0.0f,1.0f,0.0f); //green
+                   gl.glVertex2d(y, x);
+                   gl.glEnd();
+               }else if(zone == 2){
+                   gl.glColor3f( 0.0f,0.0f,1.0f ); //blue
+                   gl.glVertex2d(-y, x);
+                   gl.glEnd();
+               }else if(zone == 3){
+                   gl.glColor3f( 1.0f, 0.0f, 1.0f ); //magenta
+                   gl.glVertex2d(-x, y);
+                   gl.glEnd();
+               }else if(zone == 4){
+                   gl.glColor3f( 0.5f, 0.0f, 1.0f ); //purple
+                   gl.glVertex2d(-x, -y);
+                   gl.glEnd();
+               }else if(zone == 5){
+                   gl.glColor3f( 1.0f, 1.0f, 0.0f ); //yellow
+                   gl.glVertex2d(-y, -x);
+                   gl.glEnd();
+               }else if(zone == 6){
+                   gl.glColor3f( 0.5f, 0.5f, 0.5f ); //gray
+                   gl.glVertex2d(-y, x);
+                   gl.glEnd();
+               }else if(zone == 7){
+                   gl.glColor3f( 0.0f, 0.5f, 0.5f ); //teal
+                   gl.glVertex2d(x, -y);
+                   gl.glEnd();
+               }
+          }//end while
+ 
       //gl.glBegin (GL2.GL_POINTS);//static field
  
    }
@@ -138,74 +187,74 @@ public class slope_independent_line implements GLEventListener{
    //Convert back to correct zones
    /*
    public void convBack(int x, int y, int get_zone){
-	   if(get_zone == 0){
-		   drawPixel(x, y);
-	   }else if(get_zone == 1){
-		   
-	   }else if(get_zone == 2){
-		   
-	   }else if(get_zone == 3){
-		   
-	   }else if(get_zone == 4){
-		   
-	   }else if(get_zone == 5){
-		   
-	   }else if(get_zone == 6){
-		   
-	   }else if(get_zone == 7){
-		   
-	   }
+       if(get_zone == 0){
+           drawPixel(x, y);
+       }else if(get_zone == 1){
+           
+       }else if(get_zone == 2){
+           
+       }else if(get_zone == 3){
+           
+       }else if(get_zone == 4){
+           
+       }else if(get_zone == 5){
+           
+       }else if(get_zone == 6){
+           
+       }else if(get_zone == 7){
+           
+       }
    }
  
    
    
    //drawPixel
    public void drawPixel(int x, int y){
-	   //gl.glBegin (GL2.GL_POINTS);
-	   
+       //gl.glBegin (GL2.GL_POINTS);
+       
    }
      */
    
    //zone determination
    public int det_zone(double x0, double y0, double x1, double y1){
-	   double dx, dy;
-	   dx = x1- x0;
-	   dy = y1 - y0;
-	   if(Math.abs(dx) > Math.abs(dy)){
-	    	if(dx >= 0 && dy >= 0){
-	    		return 0;
-	    	}
-	    	else if (dx >= 0 && dy < 0){
-	    		return 7;
-	    	}
-	    	else if (dx < 0 && dy >= 0){
-	    		return 3;
-	    	}
-	    	else if (dx < 0 && dy < 0){
-	    		return 4;
-	    	}
-	    }else{
-	    	if(dx >= 0 && dy >= 0){
-	    		return 1;
-	    	}
-	    	else if (dx >= 0 && dy < 0){
-	    		return 6;
-	    	}
-	    	else if (dx < 0 && dy >= 0){
-	    		return 2;
-	    	}
-	    	else if (dx < 0 && dy < 0){
-	    		return 5;
-	    	}    	  
-	      }
-	   return -1;
+       double dx, dy;
+       dx = x1- x0;
+       dy = y1 - y0;
+       if(Math.abs(dx) > Math.abs(dy)){
+            if(dx >= 0 && dy >= 0){
+                return 0;
+            }
+            else if (dx >= 0 && dy < 0){
+                return 7;
+            }
+            else if (dx < 0 && dy >= 0){
+                return 3;
+            }
+            else if (dx < 0 && dy < 0){
+                return 4;
+            }
+        }else{
+            if(dx >= 0 && dy >= 0){
+                return 1;
+            }
+            else if (dx >= 0 && dy < 0){
+                return 6;
+            }
+            else if (dx < 0 && dy >= 0){
+                return 2;
+            }
+            else if (dx < 0 && dy < 0){
+                return 5;
+            }          
+          }
+       return -1;
    }
    
    
    public void dispose(GLAutoDrawable arg0) {
       //method body
    }
-
+ 
    
    public void init(GLAutoDrawable drawable) {
       // method body
